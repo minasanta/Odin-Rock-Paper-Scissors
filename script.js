f38f4c0e19c2fa046c229d46865b888e1c2a2167
minasanta <minasanta77@gmail.com>
2022-05-07 00:51:30 +0200

    final edit

--- script.js
+++ script.js
@@ -36,15 +36,27 @@ let count = 0;
 const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
+const pChoice = document.getElementById('ch');
 function game(){
     if(rock)
 {
     rock.addEventListener('click',() => {
         if(playRound('rock',computerPlay())=='c')
+        {
             computerScore++;
+            pChoice.textContent = 'Your choice is Rock and the computer choice is Paper so he wins';
+        }
         else if(playRound('rock',computerPlay())=='p')
+        {
+            pChoice.textContent = 'Your choice is Rock and the computer choice is Scissors so you wins';
             playerScore++;
+        }
+        else
+        {
+            pChoice.textContent = 'You both choice Rock';
+        }
     count++;
+    textRounds.textContent = `number of rounds is ${count}`;
     inTextPlayer.textContent = `The player score is ${playerScore}`;
     inTextComputer.textContent = `The computer score is ${computerScore}`;
     if(count===5)
@@ -55,59 +67,95 @@ function game(){
             finalReslut.textContent = `The winner is the computer`;
         playerScore=0;
         computerScore=0;
+        count= 0;
     }
+    else
+        {
+            finalReslut.textContent = ``;
+        }
 });
 }
 if(paper)
 {
     paper.addEventListener('click',() => {
         if(playRound('paper',computerPlay())=='c')
+        {
             computerScore++;
+            pChoice.textContent = 'Your choice is Paper and the computer choice is Scissors so he wins';
+        }
         else if(playRound('paper',computerPlay())=='p')
+        {
             playerScore++;
-    count++;
-    inTextPlayer.textContent = `The player score is ${playerScore}`;
-    inTextComputer.textContent = `The computer score is ${computerScore}`;
-    if(count===5)
-    {
-        if(playerScore>computerScore)
-            finalReslut.textContent = `The winner is the player`;
+            pChoice.textContent = 'Your choice is Paper and the computer choice is Rock so you wins';
+        }
         else
-            finalReslut.textContent = `The winner is the computer`;
-        playerScore=0;
-        computerScore=0;
-    }
+        {
+            pChoice.textContent = 'You both choice Paper';
+        }
+        count++;
+        textRounds.textContent = `number of rounds is ${count}`;
+        inTextPlayer.textContent = `The player score is ${playerScore}`;
+        inTextComputer.textContent = `The computer score is ${computerScore}`;
+        if(count===5)
+        {
+            if(playerScore>computerScore)
+                finalReslut.textContent = `The winner is the player`;
+            else
+                finalReslut.textContent = `The winner is the computer`;
+            playerScore=0;
+            computerScore=0;
+            count= 0;
+        }
+        else
+        {
+            finalReslut.textContent = ``;
+        }
 });
 }
 if(scissors)
 {
     scissors.addEventListener('click',() => {
         if(playRound('scissors',computerPlay())=='c')
+        {
+            pChoice.textContent = 'Your choice is Scissors and the computer choice is Rock so he wins';
             computerScore++;
+        }
         else if(playRound('scissors',computerPlay())=='p')
+        {
             playerScore++;
-    count++;
-    inTextPlayer.textContent = `The player score is ${playerScore}`;
-    inTextComputer.textContent = `The computer score is ${computerScore}`;
-    if(count===5)
-    {
-        if(playerScore>computerScore)
-            finalReslut.textContent = `The winner is the player`;
+            pChoice.textContent = 'Your choice is Scissors and the computer choice is Paper so you wins';
+        }
         else
-            finalReslut.textContent = `The winner is the computer`;
-        playerScore=0;
-        computerScore=0;
-    }
+        {
+            pChoice.textContent = 'You both choice Scissors';
+        }
+        count++;
+        textRounds.textContent = `number of rounds is ${count}`;
+        inTextPlayer.textContent = `The player score is ${playerScore}`;
+        inTextComputer.textContent = `The computer score is ${computerScore}`;
+        if(count===5)
+        {
+            if(playerScore>computerScore)
+                finalReslut.textContent = `The winner is the player`;
+            else
+                finalReslut.textContent = `The winner is the computer`;
+            playerScore=0;
+            computerScore=0;
+            count= 0;
+        }
+        else
+        {
+            finalReslut.textContent = ``;
+        }
 });
 }
-}game();
-let inTextPlayer = document.createElement('p');
+}
+const textRounds = document.querySelector('.rounds');
+const inTextPlayer = document.querySelector('.player-score');
 const divPlayer = document.getElementById('player');
-divPlayer.appendChild(inTextPlayer);
-let inTextComputer = document.createElement('p');
+const inTextComputer = document.querySelector('.computer-score');
 const divComputer = document.getElementById('computer');
-divComputer.appendChild(inTextComputer);
-
 const divFinal = document.getElementById('final');
 let finalReslut = document.createElement('p');
 divFinal.appendChild(finalReslut);
+game();
